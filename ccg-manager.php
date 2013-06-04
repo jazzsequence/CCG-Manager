e55962be72339fe6efbfa7e0ef94b3936f457ad6
chris reynolds <chris@jazzsequence.com>
2013-06-03 18:30:48 -0600

    initial commit. just the plugin header and license

---
+++ ccg-manager.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: CCG Manager
+Description: A WordPress plugin to manage your <abbr title="Collectable Card Game">CCG</abbr> collection.
+Author: Chris Reynolds
+Author URI: http://chrisreynolds.io
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Version: 0.1
+*/
+
+/*
+ 	Copyright (C) 2013 Chris Reynolds | hello@chrisreynolds.io
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
